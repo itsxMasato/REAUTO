9631ddc8d7a7050c79e86a3188fda2ab23c603fa
Masato <104467712+itsxMasato@users.noreply.github.com>
2025-03-28 14:33:26 -0600

    Add files via upload

---
+++ CD_Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Capa_Datos
+{
+    internal static class CD_Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Diseno());
+        }
+    }
+}
